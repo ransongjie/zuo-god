3e43c397267aeb007fe8c7706bfb891f02f43a80
jason yien <jason.yien@outlook.com>
2023-04-04 11:44:10 +0800

    link

---
+++ src/com/xcrj/link/Node.java
@@ -0,0 +1,16 @@
+package com.xcrj.link;
+
+public class Node {
+    int val;
+    Node next;
+    public Node() {
+        
+    }
+    public Node(int val) {
+        this.val=val;
+    }
+    public Node(int val,Node next) {
+        this.val=val;
+        this.next=next;
+    }
+}
